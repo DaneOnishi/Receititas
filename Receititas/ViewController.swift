82413aedf02d810aa0d0edfdcedada328fedde78
DaneOnishi <daniellaonishi07@gmail.com>
2021-07-12 13:45:48 -0300

    Initial Commit

---
+++ Receititas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Receititas
+//
+//  Created by Daniella Onishi on 12/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
