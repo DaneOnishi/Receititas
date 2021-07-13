9f04138cd00d3c2c7751f12b1a3b5a511be4cbd6
DaneOnishi <daniellaonishi07@gmail.com>
2021-07-13 13:30:40 -0300

    Creates second Screen and Adds ReceitaViewController

---
+++ Receititas/ReceitaViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ReceitaViewController.swift
+//  Receititas
+//
+//  Created by Daniella Onishi on 13/07/21.
+//
+
+import UIKit
+
+class ReceitaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    @IBAction func onBackButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion:    nil)
+    }
+    
+
+}
